2bdee999837b40e1f691b0a1567439ce1d43a75d
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2016-09-13 12:13:32 -0700

    Update the script read the device names correctly and back up all devices. Change to store snapshots for 30 days. Fix formatting and labels.

--- README.md
+++ README.md
@@ -2,29 +2,14 @@
 
 ## Overview
 * Takes daily snapshot of primary disk without any user input
-* Deletes all snapshots that are older than 7 days
+* Deletes all snapshots that are older than 30 days
 
 ## Prerequisite
 * cURL must be installed
-* The VM must have the sufficient gcloud permissions, including "compute" set to "enabled": http://stackoverflow.com/questions/31905966/gcloud-compute-list-networks-error-some-requests-did-not-succeed-insufficie#31928399
-
-## Limitations
-* Only works for the primary disk on VM
+* The VM must have the sufficient gcloud permissions. e.g. roles/compute.storageAdmin
 
 ## Setup
-* I run the script from cron.d: `0 05 * * * root sh /1/snapshot.sh >> /var/log/cron/snapshot.log 2>&1`
-* I created the `var/log/cron` directory for all of my cron outputs
-* I added the `var/log/cron` directory folder to logrotate: `/etc/logrotate.d/cron`
+* I run the script from cron.d: `0 * * * * root sh /snapshot.sh`
 
-```/var/log/cron/*.log {
-     daily
-     missingok
-     rotate 14
-     compress
-     notifempty
-     create 640 root adm
-     sharedscripts
- }```
- 
 ## Downloading the script and opening in Windows?
  * If you download the script and open it on a Windows machine, that may add windows character's to the file: https://github.com/Forward-Action/google-compute-snapshot/issues/1.
--- snapshot.sh
+++ snapshot.sh
@@ -5,51 +5,55 @@ export PATH=$PATH:/usr/local/bin/:/usr/bin
 # CREATE DAILY SNAPSHOT
 #
 
-# get the device name for this vm
-DEVICE_NAME="$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/disks/0/device-name" -H "Metadata-Flavor: Google")"
-
-# get the device id for this vm
-DEVICE_ID="$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/id" -H "Metadata-Flavor: Google")"
+# get the name for this vm
+INSTANCE_NAME="$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/hostname" -H "Metadata-Flavor: Google")"
+# strip out the instanc name from the fullly qualified domain name the google returns
+INSTANCE_NAME="${INSTANCE_NAME%%.*}"
 
 # get the zone that this vm is in
 INSTANCE_ZONE="$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/zone" -H "Metadata-Flavor: Google")"
-
 # strip out the zone from the full URI that google returns
 INSTANCE_ZONE="${INSTANCE_ZONE##*/}"
 
 # create a datetime stamp for filename
 DATE_TIME="$(date "+%s")"
 
-# create the snapshot
-echo "$(gcloud compute disks snapshot ${DEVICE_NAME} --snapshot-names gcs-${DEVICE_NAME}-${DEVICE_ID}-${DATE_TIME} --zone ${INSTANCE_ZONE})"
+# get the name of each attaced device
+DEVICE_LIST="$(gcloud compute disks list --filter users~${INSTANCE_NAME} --format='value(name)')"
 
+# create the snapshots
+echo "${DEVICE_LIST}" | while read DEVICE_NAME
+do
+  echo "$(gcloud compute disks snapshot ${DEVICE_NAME} --snapshot-names gcs-${DEVICE_NAME}-${DATE_TIME} --zone ${INSTANCE_ZONE})"
+done
 
 #
-# DELETE OLD SNAPSHOTS (OLDER THAN 7 DAYS)
+# DELETE OLD SNAPSHOTS (OLDER THAN 30 DAYS)
 #
 
-# get a list of existing snapshots, that were created by this process (gcs-), for this vm disk (DEVICE_ID)
-SNAPSHOT_LIST="$(gcloud compute snapshots list --regexp "(.*gcs-.*)|(.*-${DEVICE_ID}-.*)" --uri)"
+# get a list of existing snapshots, that were created by this process (gcs-), for this vm disk (INSTANCE_NAME)
+SNAPSHOT_LIST="$(gcloud compute snapshots list --regexp "(.*gcs-.*)|(.*-${INSTANCE_NAME}-.*)" --uri)"
 
 # loop through the snapshots
-echo "${SNAPSHOT_LIST}" | while read line ; do
+echo "${SNAPSHOT_LIST}" | while read line
+do
 
-   # get the snapshot name from full URL that google returns
-   SNAPSHOT_NAME="${line##*/}"
+  # get the snapshot name from full URL that google returns
+  SNAPSHOT_NAME="${line##*/}"
 
-   # get the date that the snapshot was created
-   SNAPSHOT_DATETIME="$(gcloud compute snapshots describe ${SNAPSHOT_NAME} | grep "creationTimestamp" | cut -d " " -f 2 | tr -d \')"
+  # get the date that the snapshot was created
+  SNAPSHOT_DATETIME="$(gcloud compute snapshots describe ${SNAPSHOT_NAME} | grep "creationTimestamp" | cut -d " " -f 2 | tr -d \')"
 
-   # format the date
-   SNAPSHOT_DATETIME="$(date -d ${SNAPSHOT_DATETIME} +%Y%m%d)"
+  # format the date
+  SNAPSHOT_DATETIME="$(date -d ${SNAPSHOT_DATETIME} +%Y%m%d)"
 
-   # get the expiry date for snapshot deletion (currently 7 days)
-   SNAPSHOT_EXPIRY="$(date -d "-7 days" +"%Y%m%d")"
+  # get the expiry date for snapshot deletion (currently 7 days)
+  SNAPSHOT_EXPIRY="$(date -d "-30 days" +"%Y%m%d")"
 
    # check if the snapshot is older than expiry date
-if [ $SNAPSHOT_EXPIRY -ge $SNAPSHOT_DATETIME ];
-        then
-	 # delete the snapshot
-         echo "$(gcloud compute snapshots delete ${SNAPSHOT_NAME} --quiet)"
-   fi
+  if [ $SNAPSHOT_EXPIRY -ge $SNAPSHOT_DATETIME ];
+  then
+	  # delete the snapshot
+    echo "$(gcloud compute snapshots delete ${SNAPSHOT_NAME} --quiet)"
+  fi
 done
