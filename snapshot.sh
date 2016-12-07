8fb735ad814ee133527d47af8a23419bdf98790a
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2016-12-07 14:28:01 -0800

    Send an alert email if no snapshot has been made in 25 hours.

--- snapshot.sh
+++ snapshot.sh
@@ -5,6 +5,11 @@ export PATH=$PATH:/usr/local/bin/:/usr/bin
 # CREATE DAILY SNAPSHOT
 #
 
+# Set this to maximum number of hours that could pass after the last snapshot is created.
+# If this is exceeded (i.e. no snapshot made in this time) then sends an alert email
+# to $EMAIL_TO.
+ALERT_HOURS=25
+
 # Set this to the number of days of snapshots you want to keep
 RETENTION_DAYS=32
 
@@ -34,21 +39,31 @@ done
 # DELETE OLD SNAPSHOTS (OLDER THAN 30 DAYS)
 #
 
+# remember the most recent snapshot
+NEWEST_TIMESTAMP=0
+
 # get a list of existing snapshots, that were created by this process (gcs-), for this vm disk (INSTANCE_NAME)
 SNAPSHOT_LIST="$(gcloud compute snapshots list --regexp "(.*gcs-.*)|(.*-${INSTANCE_NAME}-.*)" --uri)"
 
 # loop through the snapshots
-echo "${SNAPSHOT_LIST}" | while read line
+while read line
 do
 
   # get the snapshot name from full URL that google returns
   SNAPSHOT_NAME="${line##*/}"
 
   # get the date that the snapshot was created
-  SNAPSHOT_DATETIME="$(gcloud compute snapshots describe ${SNAPSHOT_NAME} | grep "creationTimestamp" | cut -d " " -f 2 | cut -d '-' -f 1-3 | tr -d \')"
+  SNAPSHOT_DATETIME="$(gcloud compute snapshots describe ${SNAPSHOT_NAME} | grep "creationTimestamp" | awk '{print substr($2, 2, 10) " " substr($2, 13, 12) " " substr($2, 25, 6)}')"
+
+  # record the newest timestamp found
+  SNAPSHOT_TIMESTAMP="$(date -d "${SNAPSHOT_DATETIME}" +%s)"
+  if [ $SNAPSHOT_TIMESTAMP -gt $NEWEST_TIMESTAMP ];
+  then
+    NEWEST_TIMESTAMP=$SNAPSHOT_TIMESTAMP
+  fi
 
   # format the date
-  SNAPSHOT_DATETIME="$(date -d ${SNAPSHOT_DATETIME} +%Y%m%d)"
+  SNAPSHOT_DATETIME="$(date -d "${SNAPSHOT_DATETIME}" +%Y%m%d)"
 
   # get the expiry date for snapshot deletion
   SNAPSHOT_EXPIRY="$(date -d "-$RETENTION_DAYS days" +"%Y%m%d")"
@@ -56,7 +71,13 @@ do
    # check if the snapshot is older than expiry date
   if [ $SNAPSHOT_EXPIRY -ge $SNAPSHOT_DATETIME ];
   then
-	  # delete the snapshot
+    # delete the snapshot
     echo "$(gcloud compute snapshots delete ${SNAPSHOT_NAME} --quiet)"
   fi
-done
+done <<< "$SNAPSHOT_LIST"
+
+ELAPSED_HOURS=$[$[$(date +%s) - $NEWEST_TIMESTAMP] / 3600]
+if [ $ELAPSED_HOURS -gt $ALERT_HOURS ];
+then
+  echo "Last snapshot on ${INSTANCE_NAME} was made ${ELAPSED_HOURS} hours ago." | mail -s "Snapshot Alert" $EMAIL_TO
+fi
